666130c50324be4a0f0517c0b10223c61b225434
SieChan <sie.chan@outlook.com>
2020-08-20 19:32:23 -0600

    Edited for W9B assignment for thursdays class

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
-var userAge = 21;
-var is_subscribed = true;
+//var userAge = 21;
+//var is_subscribed = true;
 
-console.log(userAge);
+var users = ["18", "19"];
+var is_subscribed = ["true", "false"];
+var arrayLength = users.length;
+
+for (var counter = 0; counter < arrayLength; counter++) {
+    console.log(users[counter]);
+}
+
+console.log(users);
 console.log(is_subscribed)
 
-if (userAge >= 18 && is_subscribed) {
+if (users >= 18 && is_subscribed) {
     console.log("User is 18 or older and subscribed!");
-} else if (userAge <= 18 && is_subscribed) {
+} else if (users <= 18 && is_subscribed) {
     console.log("User is under 18 and subscribed!");
-} else if (userAge >= 18 && !is_subscribed) {
+} else if (users >= 18 && !is_subscribed) {
     console.log("User is 18 or older and not subscribed!");
-} else if (userAge <= 18 && !is_subscribed) {
+} else if (users <= 18 && !is_subscribed) {
     console.log("User is under 18 and not subscribed!");
 } else {
     console.log("There was an error");
