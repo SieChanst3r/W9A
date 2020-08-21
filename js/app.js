cfd8ede5beb5da5c0bfb43879214b5f8574f3c80
SieChan <sie.chan@outlook.com>
2020-08-20 20:56:59 -0600

    Edited errors for W9B assignment for thursdays class

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
 //var userAge = 21;
 //var is_subscribed = true;
 
-var users = ["18", "19"];
-var is_subscribed = ["true", "false"];
-var arrayLength = users.length;
+var userAge = [16, 17, 18, 21, 14, 20, 13];
+var is_subscribed = [true, false, true, false, true, false, true];
 
-for (var counter = 0; counter < arrayLength; counter++) {
-    console.log(users[counter]);
+
+for (var counter = 0; counter < userAge.length; counter++) {
+    console.log(userAge[counter]);
 }
 
-console.log(users);
-console.log(is_subscribed)
 
-if (users >= 18 && is_subscribed) {
+
+if (userAge[counter] >= 18 && is_subscribed[counter] != true) {
     console.log("User is 18 or older and subscribed!");
-} else if (users <= 18 && is_subscribed) {
+} else if (userAge[counter] <= 18 && is_subscribed[counter] != true) {
     console.log("User is under 18 and subscribed!");
-} else if (users >= 18 && !is_subscribed) {
+} else if (userAge[counter] >= 18 && !is_subscribed[counter] == true) {
     console.log("User is 18 or older and not subscribed!");
-} else if (users <= 18 && !is_subscribed) {
+} else if (userAge[counter] <= 18 && !is_subscribed[counter] == true) {
     console.log("User is under 18 and not subscribed!");
 } else {
     console.log("There was an error");
